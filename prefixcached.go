028535f88d0706135b3c6c32b3533e2906f87534
Brian Trammell <brian@trammell.ch>
2017-01-12 17:05:04 +0100

    first run checkpoint. need to learn how to write prettier json parsing code...

--- prefixcached.go
+++ prefixcached.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"encoding/json"
-	"flag"
+	"errors"
+	//"flag"
+	"fmt"
 	"io"
+	"log"
 	"net"
-	"strconv"
+	"net/http"
+	"net/url"
+	//"strconv"
 )
 
 type PrefixInfo struct {
@@ -21,14 +26,17 @@ type PrefixMap map[string]PrefixInfo
 const ripeStatPrefixURL = "https://stat.ripe.net/data/prefix-overview/data.json"
 const ripeStatGeolocURL = "https://stat.ripe.net/data/geoloc/data.json"
 
-func parseRipeStatPrefixOverview(data []byte, out PrefixInfo) (err error) {
+func parseRipeStatPrefixOverview(body io.ReadCloser, out *PrefixInfo) (err error) {
 
-	var doc map[string]interface{}
+	doc := make(map[string]interface{})
 
-	err = json.Unmarshal(data, doc)
+	dec := json.NewDecoder(body)
+	err = dec.Decode(&doc)
 	if err != nil {
 		return
 	}
+
+	log.Printf("got prefix doc %#v", doc)
 
 	datax, ok := doc["data"]
 	if !ok {
@@ -46,20 +54,26 @@ func parseRipeStatPrefixOverview(data []byte, out PrefixInfo) (err error) {
 
 	out.Prefix = data["resource"].(string)
 
-	asarrayx, ok := doc["asns"]
+	asarrayx, ok := data["asns"]
 	if ok {
-		asarray = asarrayx.([]map[string]interface{})
-		out.ASN = strconv.Atoi(asarray[0]["asn"].(string))
+		asarray := asarrayx.([]interface{})
+		out.ASN = int(asarray[0].(map[string]interface{})["asn"].(float64))
 	}
+
+	return
 }
 
-func parseRipeStatGeoloc(data []byte, out PrefixInfo) (err error) {
-	var doc map[string]interface{}
+func parseRipeStatGeoloc(body io.ReadCloser, out *PrefixInfo) (err error) {
 
-	err = json.Unmarshal(data, doc)
+	doc := make(map[string]interface{})
+
+	dec := json.NewDecoder(body)
+	err = dec.Decode(&doc)
 	if err != nil {
 		return
 	}
+
+	log.Printf("got geoloc doc %#v", doc)
 
 	datax, ok := doc["data"]
 	if !ok {
@@ -71,19 +85,75 @@ func parseRipeStatGeoloc(data []byte, out PrefixInfo) (err error) {
 	pfx := data["resource"].(string)
 	if len(out.Prefix) == 0 {
 		out.Prefix = pfx
-	} else if pfx != out.Prefix {
-		err = errors.New("prefix mismatch in geoloc")
 	}
 
-	locarrayx, ok := doc["locations"]
+	locarrayx, ok := data["locations"]
 	if ok {
-		locarray = locarrayx.([]map[string]interface{})
-		out.CountryCode = asarray[0]["country"].(string)
+		locarray := locarrayx.([]interface{})
+		out.CountryCode = locarray[0].(map[string]interface{})["country"].(string)
 	}
+
+	return
 }
 
 func lookupRipeStat(addr net.IP) (out PrefixInfo, err error) {
 
+	v := make(url.Values)
+
+	v.Add("resource", addr.String())
+
+	// step 1, prefix overview
+	// add query string
+	var prefixUrl *url.URL
+	prefixUrl, err = url.Parse(ripeStatPrefixURL)
+	if err != nil {
+		return
+	}
+	prefixUrl.RawQuery = v.Encode()
+
+	// make API call
+	var resp *http.Response
+	resp, err = http.Get(prefixUrl.String())
+	if err != nil {
+		return
+	}
+
+	// parse output
+	err = parseRipeStatPrefixOverview(resp.Body, &out)
+	if err != nil {
+		return
+	}
+
+	// step 2, geolocation
+	// add query string
+	prefixUrl, err = url.Parse(ripeStatGeolocURL)
+	if err != nil {
+		return
+	}
+	prefixUrl.RawQuery = v.Encode()
+
+	// make API call
+	resp, err = http.Get(prefixUrl.String())
+	if err != nil {
+		return
+	}
+
+	// parse output
+	err = parseRipeStatGeoloc(resp.Body, &out)
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+func main() {
+	pi, err := lookupRipeStat(net.ParseIP("5.148.172.66"))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Printf("%s is in %s (AS %d) (CC %2s)\n", "5.148.172.66", pi.Prefix, pi.ASN, pi.CountryCode)
 }
 
 // API frontend
